10355c3eb374371ddf63313038e52675b55d8d91
c004245 <c004245@naver.com>
2024-11-10 23:33:40 +0900

    chore: dummy bottom

--- app/src/main/java/com/example/pet_grow_daily/feature/main/MainScreen.kt
+++ app/src/main/java/com/example/pet_grow_daily/feature/main/MainScreen.kt
@@ -2,18 +2,35 @@ package com.example.pet_grow_daily.feature.main
 
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.navOptions
+import com.example.pet_grow_daily.R
 import com.example.pet_grow_daily.feature.home.navigation.homeNavGraph
 import com.example.pet_grow_daily.feature.main.splash.navigation.splashNavGraph
 import com.example.pet_grow_daily.ui.theme.PetgrowdailyTheme
@@ -28,7 +45,9 @@ internal fun MainScreen(
     Scaffold(
         content = { paddingValues ->
             Box(
-                modifier = Modifier.fillMaxSize().padding(paddingValues)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
             ) {
                 NavHost(
                     navController = navigator.navController,
@@ -55,9 +74,81 @@ internal fun MainScreen(
                     )
                 }
             }
+        },
+//        floatingActionButton = {
+//            FloatingActionButton(
+//                onClick = { },
+//                containerColor = Color(0xFF5B64E9), // 버튼의 배경 색상
+//                contentColor = Color.White,
+//                modifier = Modifier
+//                    .offset(y = (-16).dp) //
+//            ) {
+//                Icon(
+//                    painter = painterResource(id = R.drawable.ic_add_floating),
+//                    contentDescription = "Add Icon"
+//                )
+//            }
+//
+//        },
+//        floatingActionButtonPosition = FabPosition.Center,
+        bottomBar = {
+            CustomBottomBar()
         }
     )
 
+}
+
+@Composable
+fun CustomBottomBar() {
+    BottomAppBar(
+
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // 첫 번째 아이템
+            IconButton(
+                modifier = Modifier.weight(1f),
+                onClick = { /* TODO: Add action for first item */ }) {
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_daily_grow),
+                        contentDescription = "Today's Growth"
+                    )
+                    Text("오늘의 성장")
+                }
+            }
+
+            // 두 번째 아이템
+            IconButton(
+                modifier = Modifier.weight(1f),
+                onClick = { /* TODO: Add action for second item */ }) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_total),
+                        contentDescription = "Total View"
+                    )
+                    Text("토탈 뷰")
+                }
+            }
+
+            // 세 번째 아이템
+            IconButton(
+                modifier = Modifier.weight(1f),
+                onClick = { /* TODO: Add action for third item */ }) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_total),
+                        contentDescription = "Profile"
+                    )
+                    Text("프로필")
+                }
+            }
+        }
+    }
 }
 
 @Preview
